b7caabaa0fb44282a34fa43c4054babb5513546a
Liu Jiacheng <liujiacheng@stu.scu.edu.cn>
2024-09-11 19:44:14 +0800

    create

---
+++ Bdus1/src/test/java/org/example/bdus1/Bdus1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bdus1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bdus1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f36e382617a584bf54eae9db1d76b0bac79781bb
rikka <2448598655@qq.com>
2024-09-11 23:35:59 +0800

    框架搭建v0.2

--- Bdus2/src/main/java/org/example/bdus2/Bdus2Application.java
+++ Bdus2/src/main/java/org/example/bdus2/Bdus2Application.java
@@ -2,8 +2,10 @@ package org.example.bdus2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Bdus2Application {
 
     public static void main(String[] args) {

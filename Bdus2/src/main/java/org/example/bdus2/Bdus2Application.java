ed5a9c6999a1ff709874bdf0150c468bec2ddf85
rikka <2448598655@qq.com>
2024-09-11 19:53:39 +0800

    框架搭建

---
+++ Bdus2/src/main/java/org/example/bdus2/Bdus2Application.java
@@ -0,0 +1,13 @@
+package org.example.bdus2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bdus2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bdus2Application.class, args);
+    }
+
+}

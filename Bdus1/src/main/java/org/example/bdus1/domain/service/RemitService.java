095c871677173c47a0e4ed4c07cad820d005578a
Rikka <244859865@qq.com>
2024-09-27 08:58:28 +0800

    开始优化service结构，删除api包

--- Bdus1/src/main/java/org/example/bdus1/domain/service/RemitService.java
+++ Bdus1/src/main/java/org/example/bdus1/domain/service/RemitService.java
@@ -1,11 +1,5 @@
 package org.example.bdus1.domain.service;
 
-import org.example.bdus1.domain.mapper.CustomerAccountInfoMdoMapper;
-import org.example.bdus1.domain.mapper.RmrgtLifecycleMdoMapper;
-import org.example.bdus1.domain.mapper.RmrgtRegInMdoMapper;
-import org.example.bdus1.domain.mapper.RmrgtRegOutMdoMapper;
-import org.example.common.utils.SnowflakeIdWorker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
